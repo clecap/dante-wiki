24a627c6b91d447869e46af65958287381225d30
clecap <clemens.cap@uni-rostock.de>
2023-11-07 17:34:35 +0100

    MP:

--- volumes/full/spec/script-library.sh
+++ volumes/full/spec/script-library.sh
@@ -285,6 +285,59 @@ printf "DONE: Exit code of addDatabase generated database call: ${EXIT_CODE}\n\n
 
 
 
+
+
+function injectKeys () {
+
+  # directory where we look for keys
+  local KEY_DIR=${TOP_DIR}/../KEYS-AND-CERTIFICATES
+
+  local PRIVATE_KEY="$KEY_DIR/$1.key"
+  local PUBLIC_KEY="$KEY_DIR/$1.pem"
+
+  # name of the container into which we should copy this in
+  LAP_CONTAINER=my-lap-container
+
+  printf "*** Setting up public key infrastructure, if present\n\n"
+    if [ -f $PRIVATE_KEY ]; then
+      chmod 400 ${PRIVATE_KEY}
+      printf "*** Found a private key at ${PRIVATE_KEY}, copying it in and fixing permissions ... \n" 
+      docker cp $PRIVATE_KEY    $LAP_CONTAINER:/etc/ssl/apache2/server.key
+      docker exec -it $LAP_CONTAINER   chown root.root /etc/ssl/apache2/server.key
+      docker exec -it $LAP_CONTAINER   chmod 400 /etc/ssl/apache2/server.key
+      printf "DONE\n\n"
+    else
+      printf "%b" "\e[1;31m *** ERROR: Found no private key, checked at ${PRIVATE_KEY} *** *** \e[0m"
+      exit 1
+    fi
+    if [ -f $PUBLIC_KEY ]; then
+      printf "*** Found a public key at ${PUBLIC_KEY}, copying it in and fixing permissions ... \n" 
+      chmod 444 ${PUBLIC_KEY}
+      docker cp $PUBLIC_KEY $LAP_CONTAINER:/etc/ssl/apache2/server.pem
+      docker exec -it $LAP_CONTAINER   chown root.root /etc/ssl/apache2/server.pem
+      docker exec -it $LAP_CONTAINER   chmod 444 /etc/ssl/apache2/server.pem
+      printf "DONE\n\n"
+    else
+      printf "%b" "\e[1;31m *** ERROR: Found no private key, checked at ${PRIVATE_KEY} *** *** \e[0m"
+      exit 1
+  fi
+}
+
+
+
+
+
+function apacheRestartDocker () {  # restart the apaches
+  printf "*** Killing apaches and waiting 10 seconds for processes to settle\n"
+    docker exec -it $LAP_CONTAINER  killall httpd
+    sleep 10
+  printf "DONE\n\n"
+
+  printf "*** Restarting apaches\n"
+    docker exec -it $LAP_CONTAINER  httpd
+  printf "DONE\n\n"
+}
+
 cleanUpVolume () { # Code to clean up this directory
   printf "\n*** Cleaning up volume at ${TOP_DIR} \n\n"
   # git somteimes produces awkward permissions
@@ -311,7 +364,7 @@ function removeLocalSettings () {  # removes the LocalSettings.php file, reasona
   printf "\n*** removeLocalSettings:\n"
   docker exec ${LAP_CONTAINER} rm -f ${MOUNT}/${VOLUME_PATH}/LocalSettings.php      # remove to have a clean start for install routines, ignore if not existant
   EXIT_CODE=$?
-  printf "DONE: Exit code of removeLocalSettings docker exec call: ${EXIT_CODE}\n"
+  printf "DONE: Exit code of removeLocalSettings docker exec call: ${EXIT_CODE}\n\n"
 }
 
 
