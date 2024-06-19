30a76d90054b474642c861fe5da305bdd54c5f41
clecap <clemens.cap@uni-rostock.de>
2024-06-19 20:05:43 +0200

    MP:

--- images/lap/src/home/dante/dantescript/init.sh
+++ images/lap/src/home/dante/dantescript/init.sh
@@ -7,9 +7,24 @@ echo " "
 echo "** THIS IS /dantescript/init.sh ***** "
 
 
-rm -f LocalSettings.php
+#######  terminate on error   TODO
+###### send mail upon completion ????
+#### favicon must be included into the thing - and at the dockerfile level ## todo
 
-runMWInstallScript 
+#### check if we are already initialized ##### TODO
+
+####### crontab entries for backup and for job queue TODO
+
+MOUNT=/var/www/html/
+TARGET=wiki-dir
+
+####### TODO: directory where to pick up the minimal initial contents
+  CONT=/home/dante/initial-contents/generic 
+
+
+
+### TODO: really ??????
+rm -f LocalSettings.php
 
 
 MEDIAWIKI_DB_HOST=my-dante-mysql
@@ -26,7 +41,7 @@ MEDIAWIKI_SITE_NAME="${MW_SITE_NAME}"
 
 
 MEDIAWIKI_SITE_SERVER=${MW_SITE_SERVER}
-MEDIAWIKI_SCRIPT_PATH="/${VOLUME_PATH}"
+MEDIAWIKI_SCRIPT_PATH="/${TARGET}"
 # TODO: make language variable inputable into script
 MEDIAWIKI_SITE_LANG=en
 MEDIAWIKI_ADMIN_USER=${WK_ADMIN_USER}
@@ -61,11 +76,12 @@ echo ""
 echo "*** CALLING MEDIAWIKI INSTALL ROUTINE"
 echo ""
 
-MOUNT=/var/www/html/
-VOLUME_PATH=/wiki-dir
 
-php ${MOUNT}${VOLUME_PATH}/maintenance/install.php \
-    --confpath        ${MOUNT}/${VOLUME_PATH} \
+
+
+
+php ${MOUNT}${TARGET}/maintenance/install.php \
+    --confpath        ${MOUNT}/${TARGET} \
     --dbname         "$MEDIAWIKI_DB_NAME" \
     --dbport         "$MEDIAWIKI_DB_PORT" \
     --dbserver       "$MEDIAWIKI_DB_HOST" \
@@ -86,10 +102,10 @@ php ${MOUNT}${VOLUME_PATH}/maintenance/install.php \
   echo ""
 
 # check if we succeeded to generate LocalSettings.php
-if [ -e "${MOUNT}/${VOLUME_PATH}/LocalSettings.php" ]; then
-  printf "\e[1;32m* SUCCESS:  ${MOUNT}/${VOLUME_PATH}/LocalSettings.php  generated \e[0m \n"
+if [ -e "${MOUNT}/${TARGET}/LocalSettings.php" ]; then
+  printf "\e[1;32m* SUCCESS:  ${MOUNT}/${TARGET}/LocalSettings.php  generated \e[0m \n"
 else
-  printf "\033[0;31m *ERROR:  Could not generate ${MOUNT}/${VOLUME_PATH}/LocalSettings.php - *** ABORTING \033[0m\n"
+  printf "\033[0;31m *ERROR:  Could not generate ${MOUNT}/${TARGET}/LocalSettings.php - *** ABORTING \033[0m\n"
 fi
 
 
@@ -105,36 +121,123 @@ printf  "DONE\n\n"
 
 
 
+
+#
+# Clone clecap/dante-delta from github
+#
+
+### CAVE: We MUST NOT clone dante-delta since we do not want a directory dante-delta but want to overwrite in volumes
+
+abort()
+{
+  printf "%b" "\e[1;31m *** *** *** ABORTED *** *** *** \e[0m"
+  exit 1
+}
+
+set -e                                  # abort execution on any error
+trap 'abort' EXIT                       # call abort on EXIT
+
+
+
+# the wiki directory into which we clone the files
+WIKI=${DIR}/../content/wiki-dir
+
+# the name of the branch to which we will clone
+BRANCH=master
+
+# the local git we use for all of this
+GIT_DIR=${WIKI}/.git
+
+# the remote repository
+REMOTE_REPO=https://github.com/clecap/dante-delta.git
+
+cd ${WIKI}
+
+
+# We must FIRST have a gitignore in place. If NOT then this triggers the scan of Visual Studio Codium,
+#   which detects all Mediawiki files as changed (too much), shuts down the rescanning and never sees the gitignore
+#
+printf "*** Pick up gitignore file from repository ..."
+  wget https://raw.githubusercontent.com/clecap/dante-delta/${BRANCH}/.gitignore
+printf "DONE\n\n"
+
+if [ -d "$GIT_DIR" ]; then
+  printf "*** Git directory ${GIT_DIR} already exists ... nothing DONE\n\n"
+else
+  printf "*** Git directory ${GIT_DIR} does not exist, initializing and setting to branch master ... \n"
+  git --git-dir=$GIT_DIR  init --initial-branch=$BRANCH
+  git --git-dir=$GIT_DIR  remote add origin $REMOTE_REPO
+###  git --git-dir=$GIT_DIR  config --local core.excludesfile ${DIR}/../../spec/git-ignore-for-delta       # configure this git to use spec/.gitignore
+##  git --git-dir=$GIT_DIR  
+  printf "DONE initializing a git\n\n"
+fi
+
+printf "*** Fetching origin ... "
+git --git-dir=$GIT_DIR --work-tree=${WIKI} fetch origin
+printf "DONE fetching origin\n\n"
+
+printf "*** Hard reset on local git ... "
+  git --git-dir=$GIT_DIR --work-tree=${WIKI}  reset --hard origin/master
+printf "DONE hard reset\n\n"
+
+printf "*** Pulling from ${BRANCH} ..."
+  git --git-dir=$GIT_DIR --work-tree=${WIKI}  pull origin master
+printf "DONE pulling \n\n"
+
+printf "*** Push once to connect..."
+  git push --set-upstream origin master
+printf "DONE pushing once\n\n"
+
+printf "\033[1;32m completed git-pull-from-delta.sh \033[0m \n"
+
+
+
+
+
+
+
+
+
+######## TODO #   docker exec ${LAP_CONTAINER} php ${MOUNT}${TARGET}/maintenance/importTextFiles.php --prefix "MediaWiki:ParsifalTemplate/" --rc --overwrite ${MOUNT}${TARGET}/extensions/Parsifal/initial-templates/*
+
+php ${MOUNT}${TARGET}/maintenance/importDump.php --namespaces '8' --debug $CONT/minimal-initial-contents.xml
+php ${MOUNT}${TARGET}/maintenance/importDump.php --namespaces '10' --debug $CONT/minimal-initial-contents.xml
+php ${MOUNT}${TARGET}/maintenance/importDump.php --uploads --debug $CONT/minimal-initial-contents.xml
+
+# main page and sidebar need a separate check in to show the proper dates
+php ${MOUNT}/${TARGET}/maintenance/importTextFiles.php --rc -s "Imported by wiki-init.sh" --overwrite --prefix "MediaWiki:" $CONT/Sidebar
+php ${MOUNT}/${TARGET}/maintenance/importTextFiles.php --rc -s "Imported by wiki-init.sh" --overwrite  $CONT/Main Page
+
+# Must do an update, since we have installed all kinds of extensions earlier
 printf "\n\n*** Doing a mediawiki maintenance update ... "
-  php ${MOUNT}${VOLUME_PATH}/maintenance/update.php
+  php ${MOUNT}/${TARGET}/maintenance/update.php
 printf "DONE"
 
 
+
+
 printf "*** Importing initial set of Parsifal templates..."
-  php maintenance/importTextFiles.php --prefix "MediaWiki:ParsifalTemplate/" --rc --overwrite extensions/Parsifal/initial-templates/*
+  php ${MOUNT}/${TARGET}/maintenance/importTextFiles.php --prefix "MediaWiki:ParsifalTemplate/" --rc --overwrite extensions/Parsifal/initial-templates/*
 printf " DONE\n"
 
-  php ${MOUNT}${VOLUME_PATH}/maintenance/importDump.php --namespaces '8' --debug assets/minimal-initial-contents.xml
-  php ${MOUNT}${VOLUME_PATH}/maintenance/importDump.php --namespaces '10' --debug assets/minimal-initial-contents.xml
-  php ${MOUNT}${VOLUME_PATH}/maintenance/importDump.php --uploads --debug assets/minimal-initial-contents.xml
 
-  # main page and sidebar need a separate check in to be up to date properly
-  php ${MOUNT}${VOLUME_PATH}/maintenance/importTextFiles.php --rc -s "Imported by wiki-init.sh" --overwrite --prefix "MediaWiki:" assets/Sidebar
-  php ${MOUNT}${VOLUME_PATH}/maintenance/importTextFiles.php --rc -s "Imported by wiki-init.sh" --overwrite assets/Main Page
 
-  printf "\n\n**** RUNNING: initSiteStats \n"
-    php ${MOUNT}${VOLUME_PATH}/maintenance/initSiteStats.php --update
-  printf "DONE\n"
+printf "\n\n**** RUNNING: initSiteStats \n"
+  php ${MOUNT}/${TARGET}/maintenance/initSiteStats.php --update
+printf "DONE\n"
 
-  printf "\n\n**** RUNNING: rebuildall \n"
-    php ${MOUNT}${VOLUME_PATH}/maintenance/rebuildall.php 
-  printf "DONE\n"
+printf "\n\n**** RUNNING: rebuildall \n"
+  php ${MOUNT}/${TARGET}/maintenance/rebuildall.php 
+printf "DONE\n"
 
-  printf "\n\n**** RUNNING: checkImages \n"
-    php ${MOUNT}${VOLUME_PATH}/maintenance/checkImages.php
-  printf "DONE\n"
+printf "\n\n**** RUNNING: checkImages \n"
+  php ${MOUNT}/${TARGET}/maintenance/checkImages.php
+printf "DONE\n"
 
-  printf "\n\n**** RUNNING: refreshFileHeaders \n"
-    php ${MOUNT}${VOLUME_PATH}/maintenance/refreshFileHeaders.php --verbose
-  printf "DONE\n"
+printf "\n\n**** RUNNING: refreshFileHeaders \n"
+  php ${MOUNT}/${TARGET}/maintenance/refreshFileHeaders.php --verbose
+printf "DONE\n"
 
+
+# touch the file LocalSettings.php to refresh the cache
+touch ${MOUNT}/${TARGET}/LocalSettings.php"
---
+++ images/lap/src/home/dante/dantescript/install-extension-github.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TOP_DIR=$1
+URL=$2
+NAME=$3
+BRANCH=$4
+
+git config --global http.postBuffer 524288000
+git config --global core.packedGitLimit 512m
+git config --global core.packedGitWindowSize 512m
+git config --global pack.deltaCacheSize 2047m
+git config --global pack.packSizeLimit 2047m
+git config --global pack.windowMemory 2047m
+
+rm -Rf ${TOP_DIR}/extensions/${NAME}
+git clone --depth 1 ${URL} --branch ${BRANCH} ${TOP_DIR}/extensions/${NAME}
+
+# Removing .git to save on space
+rm -Rf ${TOP_DIR}/extensions/${NAME}/.git
+
+#  Injecting installation into DanteDynamicInstalls
+echo "wfLoadExtension( '${NAME}' );" >> ${TOP_DIR}/DanteDynamicInstalls.php 
+
+
