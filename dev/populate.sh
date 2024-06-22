66d6fc3dfc03f8ded93e6cab616b52be3c9ec134
clecap <clemens.cap@uni-rostock.de>
2024-06-22 14:13:31 +0200

    MP:

--- dev/populate.sh
+++
@@ -1,6 +0,0 @@
-php /var/www/html/maintenance/importTextFiles.php --overwrite Main_Page
-php /var/www/html/maintenance/importTextFiles.php --overwrite Example_Page
-php /var/www/html/maintenance/importTextFiles.php --prefix "Project:"  --overwrite Privacy_policy
-php /var/www/html/maintenance/importTextFiles.php --prefix "Project:"  --overwrite About 
-php /var/www/html/maintenance/importTextFiles.php --prefix "Project:"  --overwrite General_disclaimer  
-
