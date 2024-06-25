ae3c5a0c8a96a169645e80f2101a1febbe154db8
clecap <clemens.cap@uni-rostock.de>
2024-06-25 22:42:54 +0200

    MP:

--- images/lap/src/home/dante/dantescript/run-apache-no-cache.sh
+++ images/lap/src/home/dante/dantescript/run-apache-no-cache.sh
@@ -6,15 +6,24 @@ GREEN="\e[32m"
 
 printf "*** THIS IS run-apache-no-cache.sh\n\n"
 
-printf "*** run-apache-no-cache.sh: Starting apache..."
+printf "*** run-apache-no-cache.sh: Starting fpm...\n"
   service php8.2-fpm start
   exec 1>&1 2>&2
-  apachectl -D DUMP_INCLUDES 
+printf "DONE\n"
+
+printf "\n*** run-apache-no-cache.sh: Listing apache includes...\n"
+  apachectl -D DUMP_INCLUDES
   exec 1>&1 2>&2
+printf "DONE\n"
+
+printf "\n*** run-apache-no-cache.sh: Listing apache modules...\n"
   apachectl -D DUMP_MODULES 
   exec 1>&1 2>&2
+printf "DONE\n"
+
+printf "\n*** run-apache-no-cache.sh: Starting apache NO-CACHE ...\n"
   apachectl  -D NO_CACHE start
   exec 1>&1 2>&2
-printf "DONE with starting apache"
+printf "DONE with starting apache\n"
 
 printf "*** EXITING run-apache-no-cache.sh"
