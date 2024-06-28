f62e4581d8337416388755ae0c4efa1c6c7a3271
clecap <clemens.cap@uni-rostock.de>
2024-06-29 00:23:41 +0200

    MP:

--- images/lap/src/home/dante/dantescript/get-dante.sh
+++ images/lap/src/home/dante/dantescript/get-dante.sh
@@ -36,19 +36,22 @@ if [ -f "$MOUNT/$TARGET/LocalSettings.php" ]; then
 ### Automagically injected by volume cmd.sh 
 ###
 require_once ("DanteSettings.php"); 
-EOF;
-    printf "\n*** get-dante.sh: injecting ...\n"
+EOF
+    printf "DONE"
   else
     printf "\n*** +++++++++++++++++++++++++++++ get-dante.sh: no LocalSettings.php found, cannot inject \n"
 fi
 
-
-cat <<EOF >> ${MOUNT}/$TARGET/mediawiki-PRIVATE.php
+if [ -f "$MOUNT/$TARGET/mediawiki-PRIVATE.php" ]; then
+    printf "\n*** get-dante.sh: mediawiki-PRIVATE.php already existing, skip generation\n"
+  else
+    printf "\n*** get-dante.sh: mediawiki-PRIVATE.php not found, generating it...\n"
+    cat <<EOF >> ${MOUNT}/$TARGET/mediawiki-PRIVATE.php
 <?php
 
-$wgPasswordSender = "${SMTP_FROM}";          // address of the sending email account
+\$wgPasswordSender = "${SMTP_FROM}";          // address of the sending email account
 
-$wgSMTP = [
+\$wgSMTP = [
     'host'     => '${SMTP_HOST}',                // hostname of the smtp server of the email account
     'IDHost'   => '${MY_DOMAINNAME}',            // sub(domain) of your wiki
     'port'     => ${SMTP_PORT},                  // SMTP port to be used
@@ -57,20 +60,21 @@ $wgSMTP = [
     'auth'     => true                           // shall authentisation be used
 ];
 
-$wgLocaltimezone="${MW:TIMEZONE}";
+\$wgLocaltimezone="${MW:TIMEZONE}";
 
-$DEEPL_API_KEY="${DEEPL_API_KEY}";
+\$DEEPL_API_KEY="${DEEPL_API_KEY}";
 
 // AWS data for an S3 user restricted to backup   dantebackup.iuk.one
-$wgDefaultUserOptions['aws-accesskey']       = '${AWS_ACCESS_KEY_ID}';
-$wgDefaultUserOptions['aws-secretaccesskey'] = '${AWS_SECRET_ACCESS_KEY}';
-$wgDefaultUserOptions['aws-bucketname']      =  '${AWS_BUCKETNAME}';
-$wgDefaultUserOptions['aws-region']          =  '${AWS_DEFAULT_REGION}';
-$wgDefaultUserOptions['aws-encpw']           =  '${MY-AWS_CRYPTO_PASSWORD}';
+\$wgDefaultUserOptions['aws-accesskey']       =  '${AWS_ACCESS_KEY_ID}';
+\$wgDefaultUserOptions['aws-secretaccesskey'] =  '${AWS_SECRET_ACCESS_KEY}';
+\$wgDefaultUserOptions['aws-bucketname']      =  '${AWS_BUCKETNAME}';
+\$wgDefaultUserOptions['aws-region']          =  '${AWS_DEFAULT_REGION}';
+\$wgDefaultUserOptions['aws-encpw']           =  '${MY_AWS_CRYPTO_PASSWORD}';
 
 ?>
-EOF;
-
+EOF
+    printf "DONE generating mediawiki-PRIVATE.php"
+fi
 
 
 
