17488b487635b15c135afd73d957b616c380093d
clecap <clemens.cap@uni-rostock.de>
2024-06-29 15:13:36 +0200

    MP:

---
+++ images/lap/src/home/dante/dantescript/activate-python.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$output = shell_exec('/home/dante/dantescript/activate-pytjon.sh');
+echo "We got from dantescript/activate-python.php: <pre>$output</pre>";
+
+?>
---
+++ images/lap/src/home/dante/dantescript/activate-python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Activate the python virtual environment for PyMuPDF
+# This MUST be a source.
+
+source /opt/myenv/bin/activate
