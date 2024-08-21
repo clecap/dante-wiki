a345a8c24880cbfd7edbf158317546f6bdd72e56
clecap <clemens.cap@uni-rostock.de>
2024-08-21 22:23:18 +0200

    MP:

--- images/dante-wiki/src/home/dante/dantescript/run-apache-no-cache.sh
+++ images/dante-wiki/src/home/dante/dantescript/run-apache-no-cache.sh
@@ -3,18 +3,7 @@
 source /home/dante/dantescript/common-defs.sh
 
 printf "*** THIS IS run-apache-no-cache.sh\n\n"
-
-# an error in the chown might happen in the git directory on restart
-# in THIS case a wrong permission (in git) is not missiogn critical
-# thus we disable the stop on error option which we might have inherited from earlier or caller
-set +e
-
-printf "\n*** run-apache-no-cache.sh: chown all files to www-data...\n"
-  chown -R www-data:www-data ${MOUNT}/${TARGET} ; exec 1>&1 2>&2
-printf "DONE chowning all files\n"
-
-set -e
-
+ 
 printf "*** run-apache-no-cache.sh: Starting fpm...\n"
   service php8.2-fpm start ; exec 1>&1 2>&2
 printf "DONE\n"
