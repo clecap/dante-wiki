827dead8e3a99d8da03efede4487bb2ecdf41973
clecap <clemens.cap@uni-rostock.de>
2024-10-26 22:40:49 +0200

    MP:

--- dist/get.sh
+++ dist/get.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/clecap/dante-wiki/HEAD/composer/docker-compose-development.yaml
-
+curl -fsSLO https://raw.githubusercontent.com/clecap/dante-wiki/HEAD/composer/docker-compose-development.yaml
+curl -fsSLO https://raw.githubusercontent.com/clecap/dante-wiki/HEAD/dist/run.sh
+mkdir private
+chmod 700 private
+touch private/configuration.sh
 docker pull clecap/dante-wiki:latest
