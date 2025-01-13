0c6d034204b45b24ed6465e8c5c73af4f155d858
clecap <clemens.cap@uni-rostock.de>
2025-01-13 08:43:25 +0100

    MP:

--- dist/get.sh
+++ dist/get.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+printf "\nThis is dante-wiki/dist/get.sh version 2.3\n\n"  # As freshness check during development
+
 # get directory where this script resides wherever it is called from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_DIR=${DIR}
+printf "TOP_DIR used is ${TOP_DIR}\n"
+printf "DIR used is ${DIR}\n"
 
 # CAVE: TOP_DIR in get is the manually generated directory into which we install  
 
