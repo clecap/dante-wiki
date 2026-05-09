cc6428232e28d92f57b18848ea4166f4986757c2
Clemens H. Cap <clemens.cap@uni-rostock.de>
2026-05-09 23:34:30 +0200

    Ad

--- composer/pull-run-raw.sh
+++ composer/pull-run-raw.sh
@@ -10,22 +10,28 @@ source $DIR/library.sh
 set -e
 trap 'abort' ERR
 
-# read in the active configuration
-source ${TOP_DIR}/private/configuration.sh
+
 
 # getImageInfo ${DANTE_IMAGE}
 
 # shutdown all running services of this kind after displaying a confirmation (to ensure git has been pushed)
+## probably no confirmation since this is for remote - or is it not ??? TODO
 askConfirmation
 downAllServices $TOP_DIR/composer/docker-compose-development.yaml
 
 pullBySpec
+
+mkdir -p private && chmod 755 private
 
 
 export CONFIG_ENCRYPTED_URL="https://iuk.one/configuration-iuk-stage.sh.enc"
 read -s -p "Password: " CONFIG_DECRYPTION_KEY && export CONFIG_DECRYPTION_KEY
 
 docker compose -f composer/docker-compose-development.yaml run --rm get-configuration
+
+
+# read in the active configuration
+source ${TOP_DIR}/private/configuration.sh
 
 
 # TODO: research if we can also do this with -d for detached mode and make better use of the health check dependency in the yaml file
